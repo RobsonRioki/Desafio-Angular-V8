3c934332bbba702d6d158dc683976bffbdb33e58
Robson Rioki Nakama <robson.nakama@sptech.school>
2024-04-19 15:07:04 -0300

    90% da tela de reuniao admin

---
+++ SilentPool/src/app/shared/container-pergunta/container-pergunta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container-pergunta',
+  templateUrl: './container-pergunta.component.html',
+  styleUrl: './container-pergunta.component.css'
+})
+export class ContainerPerguntaComponent {
+
+}
