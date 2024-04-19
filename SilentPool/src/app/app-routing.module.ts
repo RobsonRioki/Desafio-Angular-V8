9bcdd71fcdd5efda7e3d44bacb92cf9c60b34306
Robson Rioki Nakama <robson.nakama@sptech.school>
2024-04-19 10:52:43 -0300

    arrumando o app-container

--- SilentPool/src/app/app-routing.module.ts
+++ SilentPool/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { TelaAdminComponent } from './pages/tela-admin/tela-admin.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'tela-admin',
+    component: TelaAdminComponent
   }
 ];
 
