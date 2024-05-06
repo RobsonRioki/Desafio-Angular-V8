72227ec557e4d96fd8c903d900a6bbae4640a212
Robson Rioki Nakama <robson.nakama@sptech.school>
2024-05-06 12:43:28 -0300

    Arrumando a lista de perguntas por ordem de vote

--- SilentPool/src/app/pages/tela-admin/tela-admin.component.ts
+++ SilentPool/src/app/pages/tela-admin/tela-admin.component.ts
@@ -3,29 +3,29 @@ import { QuestionService } from '../../services/question.service';
 import { DadosService } from '../../services/dados.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
-interface Question{
-  category: string
-  question: string
-  vote: number
-  isDeleted: boolean
-  isReplied: boolean
-  workspace: string
-  id: string
+interface Question {
+  category: string;
+  question: string;
+  vote: number;
+  isDeleted: boolean;
+  isReplied: boolean;
+  workspace: string;
+  id: string;
 }
 
-interface Dados{
-  createAt: number
-  name: string
-  code: number
-  participants: number
-  timeLeft: Date
-  id: string
+interface Dados {
+  createAt: number;
+  name: string;
+  code: number;
+  participants: number;
+  timeLeft: Date;
+  id: string;
 }
 
 @Component({
   selector: 'app-tela-admin',
   templateUrl: './tela-admin.component.html',
-  styleUrl: './tela-admin.component.css',
+  styleUrls: ['./tela-admin.component.css'],
 })
 export class TelaAdminComponent implements OnInit {
   categoria: string = '';
@@ -33,34 +33,45 @@ export class TelaAdminComponent implements OnInit {
   code: string = '';
   id: string = '';
 
-  constructor( private questionService: QuestionService, private  dadosService: DadosService, private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private questionService: QuestionService,
+    private dadosService: DadosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  questions: Array<Question> = []
-  dados: Array<Dados> = []
+  questions: Array<Question> = [];
+  dados: Array<Dados> = [];
 
   ngOnInit(): void {
-    
-    const codigo = this.route.snapshot.params['codigo']
-    console.log(this.code)
-    this.code = codigo
+    const codigo = this.route.snapshot.params['codigo'];
+    console.log(this.code);
+    this.code = codigo;
 
-    this.dadosService.getDados().subscribe(
-      {
-        next: (dado) =>{ 
-          this.dados = dado.filter((item:Dados)=>String(item.code) === codigo);
-          console.log(dado) 
-        }
-      }
-    )
-    this.getPerguntas()
+    this.dadosService.getDados().subscribe({
+      next: (dado) => {
+        this.dados = dado.filter(
+          (item: Dados) => String(item.code) === codigo
+        );
+        console.log(dado);
+      },
+    });
+
+    this.getPerguntas(); // Chama getPerguntas ao iniciar o componente
+
+    // Atualiza as perguntas a cada 5 segundos
+    setInterval(() => this.getPerguntas(), 5000);
   }
 
-  getPerguntas(){
-    this.questionService.getQuestions().subscribe(
-     {
-      next: question => this.questions = question.filter((item : Question) => item.workspace === String(this.code))
-     }
-    )
+  getPerguntas() {
+    this.questionService.getQuestions().subscribe({
+      next: (questions) => {
+        this.questions = questions.filter(
+          (item: Question) => item.workspace === String(this.code)
+        );
+        this.sortQuestionsByVote(); // Organiza as perguntas por votos
+      },
+    });
   }
 
   like(id: string) {
@@ -90,6 +101,9 @@ export class TelaAdminComponent implements OnInit {
     // Atualiza a lista de perguntas
     this.getPerguntas();
   }
+
+  sortQuestionsByVote() {
+    // Ordena as perguntas com base no número de votos em ordem decrescente
+    this.questions.sort((a, b) => b.vote - a.vote);
+  }
 }
-
-
--- SilentPool/src/app/pages/tela-perguntas/tela-perguntas.component.ts
+++ SilentPool/src/app/pages/tela-perguntas/tela-perguntas.component.ts
@@ -3,25 +3,23 @@ import { QuestionService } from '../../services/question.service';
 import { DadosService } from '../../services/dados.service';
 import { ActivatedRoute } from '@angular/router';
 
-
 interface Question {
-  category: string
-  question: string
-  vote: number
-  isDeleted: boolean
-  isReplied: boolean
-  workspace: string
-  id: string
+  category: string;
+  question: string;
+  vote: number;
+  isDeleted: boolean;
+  isReplied: boolean;
+  workspace: string;
+  id: string;
 }
 
-
 interface Dados {
-  createAt: number
-  name: string
-  code: number
-  participants: number
-  timeLeft: Date
-  id: string
+  createAt: number;
+  name: string;
+  code: number;
+  participants: number;
+  timeLeft: Date;
+  id: string;
 }
 
 @Component({
@@ -36,33 +34,31 @@ export class TelaPerguntasComponent implements OnInit {
   id: string = '';
   constructor(private questionService: QuestionService, private dadosService: DadosService, private route: ActivatedRoute) { }
 
-  questions: Array<Question> = []
-  dados: Array<Dados> = []
+  questions: Array<Question> = [];
+  dados: Array<Dados> = [];
 
   ngOnInit(): void {
+    const codigo = this.route.snapshot.params['codigo'];
+    console.log(codigo);
+    this.codigo = codigo;
 
-    const codigo = this.route.snapshot.params['codigo']
-    console.log(codigo)
-    this.codigo = codigo
-
-
-    this.dadosService.getDados().subscribe(
-      {
-        next: (dado) =>{ 
-          this.dados = dado.filter((item:Dados)=>String(item.code) === codigo);
-          console.log(dado) 
-        }
+    this.dadosService.getDados().subscribe({
+      next: (dado) => {
+        this.dados = dado.filter((item: Dados) => String(item.code) === codigo);
+        console.log(dado);
       }
-    )
-    this.getPerguntas()
+    });
+
+    this.getPerguntas();
   }
 
-  getPerguntas(){
-    this.questionService.getQuestions().subscribe(
-      {
-        next: question => this.questions = question.filter((item : Question) => item.workspace === String(this.codigo))
+  getPerguntas() {
+    this.questionService.getQuestions().subscribe({
+      next: question => {
+        this.questions = question.filter((item: Question) => item.workspace === String(this.codigo));
+        this.sortQuestionsByVote(); // Chama a função para organizar as perguntas por voto
       }
-    )
+    });
   }
 
   inserirPergunta(): void {
@@ -76,15 +72,18 @@ export class TelaPerguntasComponent implements OnInit {
     };
 
     this.questionService.postQuestion(novaPergunta).subscribe({
-      next: ()=>{
+      next: () => {
         console.log('Pergunta enviada com sucesso:');
         this.categoria = '';
         this.pergunta = '';
         this.getPerguntas();
       },
-      error: (error) => {console.log(error)}
-    })
+      error: (error) => {
+        console.log(error);
+      }
+    });
   }
+
   like(id: string) {
     const questionToUpdate = this.questions.find(
       (item: Question) => item.id === id
@@ -94,8 +93,14 @@ export class TelaPerguntasComponent implements OnInit {
       this.questionService.updateQuestion(questionToUpdate).subscribe({
         next: () => {
           console.log('Voto atualizado com sucesso!');
+          this.sortQuestionsByVote(); // Chama a função para organizar as perguntas por voto após a atualização do voto
         },
       });
     }
+  }
+
+  sortQuestionsByVote() {
+    // Ordena as perguntas com base no número de votos em ordem decrescente
+    this.questions.sort((a, b) => b.vote - a.vote);
   }
 }
