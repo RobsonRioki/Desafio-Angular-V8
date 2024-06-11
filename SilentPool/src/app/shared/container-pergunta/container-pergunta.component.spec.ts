1f2262542f577bb1572148bb38436dbc76c75b3d
Robson Rioki Nakama <robson.nakama@sptech.school>
2024-06-11 13:30:52 -0300

    adicionando testes no componente de perguntas

--- SilentPool/src/app/shared/container-pergunta/container-pergunta.component.spec.ts
+++ SilentPool/src/app/shared/container-pergunta/container-pergunta.component.spec.ts
@@ -1,5 +1,30 @@
-describe('HomeComponent', () => {
-  it('should create', () => {
-    expect(1).toBe(1);
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ContainerPerguntaComponent } from "./container-pergunta.component"
+import { EventEmitter } from "@angular/core";
+
+describe('#ContainerPerguntaComponent', ()=> {
+let component: ContainerPerguntaComponent;
+let fixture: ComponentFixture<ContainerPerguntaComponent>;
+let element: HTMLElement;
+
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    declarations: [ContainerPerguntaComponent],
   });
+  fixture = TestBed.createComponent(ContainerPerguntaComponent);
+  component = fixture.componentInstance;
+  element = fixture.nativeElement;
 });
+
+it('should create component', () => {
+  expect(component).toBeTruthy();
+});
+
+it('should get a true value', () =>{
+  component.isAdmin = true;
+  expect(component.isAdmin).toBe(true);
+})
+
+
+
+})
