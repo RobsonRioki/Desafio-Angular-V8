e05efe706d2da9b06e8bd0cc1f2402fdadec9e33
Luiz Fernando Meneses Pereira <luizfernandomdp14@outlook.com>
2024-06-20 11:03:55 -0300

    ajustes

--- SilentPool/src/app/shared/container-info/container-info.component.ts
+++ SilentPool/src/app/shared/container-info/container-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-container-info',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './container-info.component.css'
 })
 export class ContainerInfoComponent {
-
+  @Input() titulo: string = '';
+  @Input() descricao: string = '';
 }
