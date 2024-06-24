85ed9e47bae95f4ac78d92c0a2e0fcdc025770e6
Luiz Fernando Meneses Pereira <luizfernandomdp14@outlook.com>
2024-06-24 11:23:48 -0300

    ajustes na tela home3

--- src/app/pages/home3/components/home3-template/home3-template.component.ts
+++ src/app/pages/home3/components/home3-template/home3-template.component.ts
@@ -1,62 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ModalComponent } from '../../../../shared/modal/modal.component'; 
-import { ActivatedRoute, Router } from '@angular/router';
-import { DadosService } from '../../../../services/dados.service'; 
+import { Component } from '@angular/core';
 
-interface Dados {
-  createAt: number;
-  name: string;
-  code: number;
-  participants: number;
-  timeLeft: Date;
-  id: string;
-}
+
 
 @Component({
   selector: 'app-home3-template',
   templateUrl: './home3-template.component.html',
   styleUrls: ['./home3-template.component.css']
 })
-export class Home3TemplateComponent implements OnInit {
-  code = '';
-
-  constructor(
-    public dialog: MatDialog,
-    private router: Router,
-    private dadosService: DadosService,
-    private route: ActivatedRoute
-  ) {}
-
-  dados: Array<Dados> = [];
-
-  ngOnInit(): void {
-    this.code = this.route.snapshot.params['codigo'];
-
-    this.dadosService.getDados().subscribe({
-      next: (dado) => {
-        this.dados = dado;
-        console.log(this.dados);
-      },
-    });
-  }
-
-  onCodigoRecebido(event: { codigo: string }) {
-    this.code = event.codigo;
-    this.entrarSala();
-  }
-
-  entrarSala() {
-    const codigoExistente = this.dados.find((item) => item.code.toString() === this.code);
-
-    if (codigoExistente) {
-      this.router.navigate(['/perguntas', this.code]);
-    } else {
-      alert('Código inválido. Por favor, insira um código válido.');
-    }
-  }
-
-  openDialog() {
-    this.dialog.open(ModalComponent);
-  }
+export class Home3TemplateComponent {
 }
--- src/app/pages/home3/containers/home3/home3.component.ts
+++ src/app/pages/home3/containers/home3/home3.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../../../../shared/modal/modal.component'; 
+import { ActivatedRoute, Router } from '@angular/router';
+import { DadosService } from '../../../../services/dados.service'; 
+
+interface Dados {
+  createAt: number;
+  name: string;
+  code: number;
+  participants: number;
+  timeLeft: Date;
+  id: string;
+}
 
 @Component({
   selector: 'app-home3',
   templateUrl: './home3.component.html',
   styleUrl: './home3.component.css'
 })
-export class Home3Component {
 
+export class Home3Component implements OnInit {
+  code = '';
+
+  constructor(
+    public dialog: MatDialog,
+    private router: Router,
+    private dadosService: DadosService,
+    private route: ActivatedRoute
+  ) {}
+
+  dados: Array<Dados> = [];
+
+  ngOnInit(): void {
+    this.code = this.route.snapshot.params['codigo'];
+
+    this.dadosService.getDados().subscribe({
+      next: (dado) => {
+        this.dados = dado;
+        console.log(this.dados);
+      },
+    });
+  }
+
+  onCodigoRecebido(event: { codigo: string }) {
+    this.code = event.codigo;
+    this.entrarSala();
+  }
+
+  entrarSala() {
+    const codigoExistente = this.dados.find((item) => item.code.toString() === this.code);
+
+    if (codigoExistente) {
+      this.router.navigate(['/perguntas', this.code]);
+    } else {
+      alert('Código inválido. Por favor, insira um código válido.');
+    }
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent);
+  }
 }
