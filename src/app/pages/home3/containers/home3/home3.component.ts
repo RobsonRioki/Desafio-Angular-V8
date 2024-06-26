2a6a7b22bae6281b4b579affcdf5714c861f8b69
Luiz Fernando Meneses Pereira <luizfernandomdp14@outlook.com>
2024-06-26 14:55:21 -0300

    ajustes

--- src/app/pages/home3/containers/home3/home3.component.ts
+++ src/app/pages/home3/containers/home3/home3.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DadosService } from '../../../../services/dados.service';
+import { ModalComponent } from '../../../../shared/modal/modal.component';
 
 @Component({
   selector: 'app-home3',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './home3.component.css'
 })
 
-export class Home3Component  {
+export class Home3Component {
+
+  constructor(
+    public dialog: MatDialog,
+    private router: Router,
+    private dadosService: DadosService,
+    private route: ActivatedRoute
+  ) { }
+
+  openDialog(value: boolean) {
+    this.dialog.open(ModalComponent);
+    console.log(value)
+  }
+
 }
